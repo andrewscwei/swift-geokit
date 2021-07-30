c41d742a2c6791273f1722d421dac94c0d61e17e
Andrew Wei <andrewscwei@gmail.com>
2021-07-29 23:20:57 -0700

    Update build pipeline

--- Package.swift
+++ Package.swift
@@ -1,24 +1,45 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
+
+#if os(Linux)
+import Glibc
+#else
+import Darwin.C
+#endif
+
+enum Environment: String {
+  static let `default`: Environment = .local
+
+  case local
+  case development
+  case production
+
+  static func get() -> Environment {
+    if let envPointer = getenv("CI"), String(cString: envPointer) == "true" {
+      return .production
+    }
+    else if let envPointer = getenv("SWIFT_ENV") {
+      let env = String(cString: envPointer)
+      return Environment(rawValue: env) ?? .default
+    }
+    else {
+      return .default
+    }
+  }
+}
 
 let package = Package(
   name: "GeoKit",
   platforms: [.iOS(.v11)],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "GeoKit",
       targets: ["GeoKit"]),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "GeoKit",
       dependencies: []),
