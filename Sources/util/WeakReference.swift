fe204b9d2abd603138957157f007c02baa157c08
Andrew Wei <andrewscwei@gmail.com>
2024-11-05 12:27:52 +0900

    style: Fix formatting

--- Sources/util/WeakReference.swift
+++ Sources/util/WeakReference.swift
@@ -1,16 +1,7 @@
-/// An immutable object that holds a weak reference to a target object specified
-/// during initialization. If the target object is a value type, it will be
-/// passed around as a value regardless.
-internal struct WeakReference<T> {
+struct WeakReference<T> {
   private let getReference: () -> T?
 
-  /// Creates a new `WeakReference` instance.
-  ///
-  /// - Parameters:
-  ///   - object: The target object to store as a weak reference. If the target
-  ///             object is a value type, it will be passed around as a value
-  ///             regardless.
-  internal init(_ object: T) {
+  init(_ object: T) {
     let reference = object as AnyObject
 
     getReference = { [weak reference] in
@@ -18,11 +9,5 @@ internal struct WeakReference<T> {
     }
   }
 
-  /// Unwraps and returns the wrapped object. Since the object is weakly
-  /// referenced, there is no guarantee that the object will still exist in
-  /// memory when invoking this method (this does not apply if the wrapped
-  /// object is a value type).
-  ///
-  /// - Returns: The wrapped object (if it still exists).
-  internal func get() -> T? { getReference() }
+  func get() -> T? { getReference() }
 }
